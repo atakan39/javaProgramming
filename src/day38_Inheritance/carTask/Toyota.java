94319c4072eef7c150bc97906c2fe9f25c25e91c
atakantaskin <atakantaskin91@gmail.com>
2022-01-13 10:38:12 -0500

    Day38Inheritance

--- src/day38_Inheritance/carTask/Toyota.java
+++ src/day38_Inheritance/carTask/Toyota.java
@@ -1,7 +1,11 @@
 package day38_Inheritance.carTask;
 
 public class Toyota extends Car{
-    public Toyota(String brand, String model, int year, int miles, String color,double price) {
-        super(brand, model, year, miles, color,price);
+    public Toyota(String model, int year, int miles, String color,double price) {
+        super("Toyota", model, year, miles, color,price);
+    }
+
+    public void reliable(){
+        System.out.println(brand+" "+model+" is reliable");
     }
 }
