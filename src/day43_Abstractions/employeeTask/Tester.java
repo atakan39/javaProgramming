c6778f7b0b6b9accfaff5f29933b747bccc941f1
atakantaskin <atakantaskin91@gmail.com>
2022-01-31 16:37:27 -0500

    day42 exceptions

--- src/day43_Abstractions/employeeTask/Tester.java
+++ src/day43_Abstractions/employeeTask/Tester.java
@@ -1,6 +1,6 @@
 package day43_Abstractions.employeeTask;
 
-public class Tester extends Employee{
+public final class Tester extends Employee{
     public Tester(String name, int age, char gender, long employeeId, String jobTitle, double salary) {
         super(name, age, gender, employeeId, jobTitle, salary);
     }
